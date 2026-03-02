a5d742c9849cc8e43c459482a1b168e7f6bec25e
dvdbsh <139626660+dvdbsh@users.noreply.github.com>
2026-03-02 13:04:45 -0500

    convert to jekyll

--- scripts/ribbon-scroll.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function () {
-    requestAnimationFrame(() => {
-        const scrollPosition = window.pageYOffset;
-        const ribbonText = document.querySelector('.ribbontext');
-        ribbonText.style.transform = `translateX(${-scrollPosition * 0.2}px)`;
-    });
-});
